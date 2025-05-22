dedfd7d5e782da7152bc0535d5328872bfdc61f6
Merry Rosalie <merryrosalie001@gmail.com>
2025-05-22 17:39:15 +0700

    feat: mock signin form

---
+++ frontend/src/app/signin/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { request } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+export default function Page() {
+  const router = useRouter();
+  const [input, setInput] = useState("");
+
+  const handleOnSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const { data, error } = await request("POST", "/auth/register", {
+      leetcodeSessionCookie: input,
+    });
+
+    if (error) {
+      alert(error);
+      return;
+    }
+
+    router.push("/game");
+  };
+
+  return (
+    <form className="flex flex-col gap-2 p-2">
+      <label htmlFor="cookies-input">Leetcode Session Cookies</label>
+      <input
+        id="cookies-input"
+        type="input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="border"
+        placeholder="Enter your cookies here..."
+      />
+      <button
+        type="submit"
+        onClick={handleOnSubmit}
+        className="bg-foreground text-background"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
