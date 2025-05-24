68992b3b739308f0c5a2becb714943f1bd78631f
mony <z5520405@ad.unsw.edu.au>
2025-05-24 17:15:55 +1000

    chore: remove console.log

--- backend/src/userInfos/userStreak.ts
+++ backend/src/userInfos/userStreak.ts
@@ -5,7 +5,6 @@ const prisma = new PrismaClient();
 export async function getStreakCounter(userId: string) {
 
   const user = await prisma.user.findUnique({ where: { id: userId } });
-  console.log(user);
   if (!user) {
     return {error: "failed to Find User"};
   }
